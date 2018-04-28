c6cb27db0dd3b2a70ce5132e97c4a9bb2b7dea8b
leodir <leodirsarreta@hotmail.com>
2018-04-28 10:56:20 -0300

    primeiro commit

---
+++ pages/sobre.php
@@ -0,0 +1,58 @@
+<main>
+<section id="sobre">
+    <div class="sobre">
+		
+		<h1>King Burger</h1>
+		<p class="center descricao">
+			<br>
+			Em 1937, os irmãos Dick e Mac McDonald abrem uma barraca de cachorro-quente chamada Airdome em Arcádia, Califórnia. Durante 3 anos, já em 1940, os irmãos mudam a barraca Airdome para San Bernardino, também na Califórnia, onde eles abrem um restaurante McDonald´s na Rota 66, em 15 de Maio. O cardápio se baseava em 25 itens, a maioria deles, churrasco. 
+            O primeiro hambúrguer McDonald´s custou US$0,15, e como era comum na época, contrataram 20 garçons que sobre patins, entregavam o pedido do cliente no carro. Isso se tornou popular, e muito lucrativo,  fazendo um tremendo sucesso, e aumentando cada vez mais o número de clientes.
+		</p>
+    </div>
+    <div class="macdonald"></div>
+    <div class="sobre">
+		
+		<p class="center descricao">
+			<br>
+			Após alguns anos, passou pela empresa um funcionário que tinha uma paixão extrema por bacon e essa paixão o fez conquistar os donos do macDonalds que ao perceberem isso resolveram doar cerca de 50% das ações da empresa, esse funcionário se chamava professor Burnes e o mesmo resolveu alterar o nome da empresa para King Burger!!!
+		</p>
+    </div>    
+</section>
+</main>
+<footer>
+		<div class="container row">
+			<div class="coluna">
+				<h3>Menu de Acesso</h3>
+				<ul>
+					<li><a href="home" title="Home">Home</a></li>
+					<li><a href="sobre" title="Sobre a King Burger">Sobre</a></li>
+					<li><a href="cardapio" title="Cardápio">Cardápio</a></li>
+					<li><a href="contato" title="Contao">Contato</a></li>
+				</ul>
+			</div>
+			<div class="coluna borda">
+				<div class="fb-page" data-href="https://www.facebook.com/professorburnes/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/professorburnes/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/professorburnes/">ProfessorBurnes</a></blockquote></div>
+			</div>
+			<div class="coluna borda">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.6648497272745!2d-53.315848449780674!3d-23.759328074741354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f2d40292269565%3A0x34c0ef5f4ffa7009!2sCol%C3%A9gio+e+Faculdade+ALFA+de+Umuarama!5e0!3m2!1spt-BR!2sbr!4v1521038515751" width="100%" height="215" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+		</div>
+		<p class="center">
+			Desenvolvido por Tidinho - Todos os Direitos Reservados
+		</p>
+</footer>
+
+<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.12';
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+	$("#menu").click(function() {
+		$("nav ul").toggle();
+	})
+
+</script>
